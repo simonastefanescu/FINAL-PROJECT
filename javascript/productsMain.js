bb6aba0c3148b5de3038f4d01635566f94766a6b
gza <smnstefanescu@gmail.com>
2022-10-28 09:32:23 +0300

    products page styles updated

--- javascript/productsMain.js
+++ javascript/productsMain.js
@@ -3,7 +3,7 @@ const productList = [
         "id": 1,
         "title": "Alocasia Black Velvet",
         "price": 15,
-        "currency": "dollars",
+        "currency": "$",
         "description": "test",
         "image": "/images/product 1 alocasia black velvet/main.webp"
     }
@@ -12,7 +12,7 @@ const productList = [
         "id": 2,
         "title": "ZZ Plant",
         "price": 20,
-        "currency": "dollars",
+        "currency": "$",
         "description": "test",
         "image": "/images/product 2 zz plant/main.jpg"
     },
@@ -20,35 +20,35 @@ const productList = [
         "id": 3,
         "title": "String of hearts",
         "price": 15,
-        "currency": "dollars",
+        "currency": "$",
         "description": "test",
         "image": "/images/product 3 String of hearts/main.jpg"
     }
     , {
         "id": 4,
         "title": "Pothos Silver Satin",
-        "price": 15,
-        "currency": "dollars",
+        "price": 30,
+        "currency": "$",
         "description": "test",
         "image": "/images/product 4 Silver Satin Pothos/main.jpg"
     }
     ,
     {
         "id": 5,
-        "title": "Calathea Orbifolia",
-        "price": 15,
-        "currency": "dollars",
+        "title": "Snake Plant",
+        "price": 25,
+        "currency": "$",
         "description": "test",
-        "image": "/images/product 1 alocasia black velvet/main.jpg"
+        "image": "/images/product 5 snake plant/main.webp"
     }
     ,
     {
         "id": 6,
         "title": "Calathea Zebrina",
-        "price": 15,
-        "currency": "dollars",
+        "price": 12,
+        "currency": "$",
         "description": "test",
-        "image": "/images/product 1 alocasia black velvet/main.jpg"
+        "image": "/images/product 6 Calathea Zebrina/main.webp"
     }
     ,
 ]
@@ -76,7 +76,7 @@ for (let i = 0; i < productList.length; i++) {
 
     let price = document.createElement("p")
     price.classList.add("price")
-    price.textContent = productList[i].price
+    price.textContent = productList[i].price+ productList[i].currency
     item.appendChild(price);
 
     let button = document.createElement("button")
